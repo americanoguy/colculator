b8b2d34c22f8dbbddf0258318c5d1eb3573b32e1
americanoguy <usamericandollar@gmail.com>
2025-10-21 01:46:03 +0400

    colculator

---
+++ script.js
@@ -0,0 +1,45 @@
+const ekran = document.getElementById("Netice");
+
+function Elaveet(value){
+    ekran.innerHTML+=value;
+}
+
+function Cfunction(){
+    ekran.innerHTML="";
+}
+
+
+function reqemsilme(){
+    ekran.innerHTML=ekran.innerHTML.slice(0,-1)
+}
+
+
+function hesabla(){
+
+    //evaldan istifade etdim W3 schools ve chat-in yardimi ile
+    ekran.innerHTML = eval(ekran.innerText);
+}
+
+
+
+
+const mod = document.getElementById("changemode");
+
+let darkmode = false;
+
+function changemode() {
+    darkmode = !darkmode;
+
+    if(darkmode){
+        document.body.style.background = "#121212";
+        document.body.style.color = "white";
+        mod.style.background ="white"
+        mod.style.color ="black"
+
+    } else {
+        document.body.style.background = "white";
+        document.body.style.color = "black";
+        mod.style.background = "black"
+        mod.style.color="white"
+    }
+}
